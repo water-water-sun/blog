458317138dafa9a4befb4a8998c238e8761623f1
water-water-sun <1927138579@qq.com>
2025-11-13 19:16:24 +0800

    Site updated: 2025-11-13 19:16:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/12/hello-world/"];function toRandomPost(){
+var posts=["2025/11/12/hello-world/","2025/11/13/learn/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
